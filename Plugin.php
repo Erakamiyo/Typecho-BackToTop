af70f5c331a0fa3d13b603d946cb2b486cbafd3d
nor <90123446+Erakamiyo@users.noreply.github.com>
2023-09-26 22:57:00 +0800

    Update Plugin.php
    部分付费主题需要验证域名，不能引入公共cdn的js、css。对这一部分做了一点修改，默认使用插件本地js，同时可以修改引入地址。能力有限，代码如有误请指正。

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 东方返回顶部样式
+ * 东方返回顶部样式。<br>项目地址：<a href="https://github.com/xiamuguizhi/Typecho-BackToTop" title="Github" target="_black">xiamuguizhi/Typecho-BackToTop</a>.
  *
  * @package BackToTop
  * @author 夏目贵志
@@ -42,8 +42,12 @@ class BackToTop_Plugin implements Typecho_Plugin_Interface
      * @return void
      */
     public static function config(Typecho_Widget_Helper_Form $form) {
-        $jquery = new Typecho_Widget_Helper_Form_Element_Checkbox('jquery', array('jquery' => '禁止加载jQuery'), null, _t('Js设置'), _t('插件需要加载jQuery，如果主题模板已经引用加载JQuery，则可以勾选。'));
+	$form->addInput(new Typecho_Widget_Helper_Form_Element_Checkbox('jsSettings', NULL, NULL, _t('<h2> Js 设置 </h2>'), _t('<strong>在这里自定义你的Js设置。</strong> <br /><br /><div class="divider"></div>')));
+        
+        $jquery = new Typecho_Widget_Helper_Form_Element_Radio('jquery', array('1' => _t('是'), '0' => _t('否')), '0', _t('是否启用插件内jQuery？'), _t('本插件需要jQuery。如果主题模板已经引用加载jQuery，则应选否，避免重复加载。<strong>默认选否。</strong>'));
         $form->addInput($jquery);
+        $jQueryLinkCustom = new Typecho_Widget_Helper_Form_Element_Text('jQueryLinkCustom', NULL, NULL, _t('自定义引用jQuery地址'), _t('<strong>此选项仅在启用插件内jQuery功能后有效。可为空，默认使用插件自带jQuery (3.7.1.min.js)。</strong><br>可替换为其他位置文件或CDN文件加速。'));
+        $form->addInput($jQueryLinkCustom);
     }
     
 
@@ -93,24 +97,34 @@ class BackToTop_Plugin implements Typecho_Plugin_Interface
 			 break;
 		 }		
         $Options = Helper::options()->plugin('BackToTop');
-        $Path = Helper::options()->pluginUrl . '/BackToTop/';	
+        $Path = Helper::options()->pluginUrl . '/BackToTop/';
+	$jQueryLink = "' . $Path . 'js/ajax_libs_jquery_3.7.1.min.js";
         echo '<img id="BackToTop" src="' . $Path . 'images/'.$image_file.'" title="返回顶部~">';
-        if (!$Options->jquery || !in_array('jquery', $Options->jquery)) {
-            echo '<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>';
+	
+        if (isset($Options->jquery) && $Options->jquery == '1') {
+            if (isset($Options->jQueryLinkCustom) && $Options->jQueryLinkCustom !== NULL) {
+				echo '<script type="text/javascript" src="' . $jQueryLinkCustom . '"></script>';
+			} else {
+				echo '<script type="text/javascript" src="' . $jQueryLink . '"></script>';
+			}
         }
-        echo '	
-		<script type="text/javascript">
-		$(function(){
-		  $("#BackToTop").click(function() {
-			  $("html,body").animate({scrollTop:0}, 500);
-		  }); 
-		 })
-
-		</script>	
-		';
+	/**
+         * 原版
+         * 
+         * if (!$Options->jquery || !in_array('jquery', $Options->jquery)) {
+		 *     echo '<script type="text/javascript" src="' . $Path . 'js/ajax_libs_jquery_3.7.1.min.js"></script>';
+         * }
+         */
+	
+        echo '<script type="text/javascript">
+			$(function(){
+			  $("#BackToTop").click(function() {
+				  $("html, body").animate({scrollTop:0}, 500);
+			  }); 
+			 })
+		</script>';
     }
+	
 }
-
-
 
 
